7be4bd3e6ceeba51d4fce6b95407c814018bb556
Your Name <you@example.com>
2019-08-09 11:55:32 -0400

    got validation work for name

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -4,8 +4,9 @@ import * as Yup from "yup";
 
 
 class UserForm extends React.Component {
-    constructor (){
-        super();
+    constructor (props){
+        super(props);
+        console.log('props in UserForm', props)
         this.state = {
             name: "Hong",
             user: []
@@ -15,11 +16,13 @@ class UserForm extends React.Component {
 
 
     render(){
+        console.log('props in render', this.props)
         return (
             <div>
                 <h1>Form created by {this.state.name}</h1>
                 <Form>
                     <Field type="text" name="name" placeholder="Username" />
+                    {this.props.touched.name && this.props.errors.name && <p>{this.props.errors.name}</p>}
                     <Field type="text" name="password" placeholder="Password"/>
                 </Form>
                 <button>Submit!</button>
@@ -32,8 +35,14 @@ const UserFormik = withFormik({
     mapPropsToValues(values){
         return {
             name: values.nam || '',
+            password: values.password || '',
         }
-    }
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        password: Yup.string().min(8).required('Password is not strong')
+    })
     
 
 
