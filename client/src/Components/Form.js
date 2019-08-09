9488aeddf7af18ecfc47a7fa700a207821639f4a
Your Name <you@example.com>
2019-08-09 11:39:35 -0400

    done UserForm class component but the form has not worked yet without formik

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Form, Field, withFormik} from 'formik'
+import * as Yup from "yup";
 
 
 class UserForm extends React.Component {
     constructor (){
         super();
         this.state = {
-            name: "Hong"
+            name: "Hong",
+            user: []
         }
     }
 
@@ -15,9 +18,16 @@ class UserForm extends React.Component {
         return (
             <div>
                 <h1>Form created by {this.state.name}</h1>
+                <Form>
+                    <Field type="text" name="name" placeholder="Username" />
+                    <Field type="text" name="password" placeholder="Password"/>
+                </Form>
+                <button>Submit!</button>
             </div>
         )
     }
 }
+
+ 
 
 export default UserForm;
